83796646dfc3d372c53452dad2da306eb07d9b4f
Sandesh Rana <vimmrana0@gmail.com>
2023-06-23 20:13:52 +0545

    Add different methods for overriding field

--- example/necal_django/necal/models.py
+++ example/necal_django/necal/models.py
@@ -2,5 +2,18 @@ from django.db import models
 from django.db.models import fields
 
 
-class NepaliCalendar(fields.DateTimeField):
-    pass
+class NepaliCalendar(models.DateField):
+    # Add source/link for field customization
+    description = "Nepali Calendar Field"
+
+    def formfield(self, **kwargs):
+        pass
+
+    def from_db_value(self, value, expression, connection):
+        pass
+
+    def get_prep_value(self, value):
+        pass
+
+    def to_python(self):
+        pass
