af5671352755816f77c70f10c71e4fbe80020959
Sandesh Rana <vimmrana0@gmail.com>
2023-06-27 12:41:18 +0545

    Fix jest

--- example/necal-ts/src/counter.ts
+++ example/necal-ts/src/counter.ts
@@ -1,9 +1,16 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+// export function setupCounter(element: HTMLButtonElement) {
+//   let counter = 0
+//   const setCounter = (count: number) => {
+//     counter = count
+//     element.innerHTML = `count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
+// }
+
+function sum(a, b) {
+  return a + b;
 }
+
+module.exports = sum;
+
