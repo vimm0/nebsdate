e3a21cfe846214fc0d39cff2d8bd9a631c91b4e5
Sandesh Rana <vimmrana0@gmail.com>
2023-06-27 13:41:21 +0545

    Add new test cases

---
+++ example/necal-ts/test/constant.test.js
@@ -0,0 +1,33 @@
+import {
+    parse,
+    monthDaysMappings,
+    yearDaysMapping,
+    Language,
+    findPassedDays,
+    COMPLETED_DAYS,
+    TOTAL_DAYS,
+    getYearIndex,
+    mapDaysToDate,
+    format
+  } from '../src/constant'
+  
+  const Baisakh = 0
+  const Jestha = 1
+  const Asar = 2
+  const Shrawan = 3
+  const Bhadra = 4
+  const Aswin = 5
+  const Kartik = 6
+  const Mangsir = 7
+  const Poush = 8
+  const Magh = 9
+  const Falgun = 10
+  const Chaitra = 11
+
+  describe('mapDaysToDate', () => {
+  })
+
+  describe('format', () => {
+  })
+  describe('parse', () => {
+  })
