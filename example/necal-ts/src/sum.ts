c757b4adba0b618d69787d0ceb700809bfedf51c
Sandesh Rana <vimmrana0@gmail.com>
2023-06-29 14:19:21 +0545

    Add sum for rest params and add test

--- example/necal-ts/src/sum.ts
+++ example/necal-ts/src/sum.ts
@@ -3,3 +3,10 @@ export function sum(a: number, b: number): number {
 }
 
 
+export function sumParam(a: number, b: number, c: number): number {
+    return a + b + c;
+}
+
+export function sumRest(...numbers: number[]): number {
+    return numbers.reduce((total, current) => total + current, 0);
+}
--- example/necal-ts/test/sum.test.ts
+++ example/necal-ts/test/sum.test.ts
@@ -1,6 +1,25 @@
-import {expect, test} from '@jest/globals';
-import {sum} from '../src/sum'
+import { describe, it, expect } from '@jest/globals';
+import { sum, sumParam, sumRest } from '../src/sum'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('sum function', () => {
+  it('should return addition of numbers', () => {
+    expect(sum(1, 2)).toBe(3);
+    expect(sumParam(1, 2, 3)).toBe(6);
+    expect(sumRest(1, 2, 3)).toBe(6);
+
+  });
+  it('should return the correct sum for multiple numbers', () => {
+    expect(sumRest(1, 2, 3)).toBe(6);
+    expect(sumRest(4, 5, 6, 7)).toBe(22);
+    expect(sumRest(-1, 2, -3, 4)).toBe(2);
+  });
+
+  it('should return the number itself if only one number is provided', () => {
+    expect(sumRest(10)).toBe(10);
+    expect(sumRest(-5)).toBe(-5);
+  });
+
+  it('should return 0 if no arguments are provided', () => {
+    expect(sumRest()).toBe(0);
+  });
 });
